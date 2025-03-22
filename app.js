4d8c8c11531f89bd31e19267b2117c54a2183e86
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-22 18:28:12 +0100

    Add static file serving middleware and initial CSS styles

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware.
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.status(200).send('Home page');
 });
