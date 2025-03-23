99d139f180d57df05f3b7fa4f519c626f947a987
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-23 18:56:22 +0100

    Serve HTML files for home and about routes

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Middleware.
 app.use(express.static('public'));
 
+// Home page.
 app.get('/', (req, res) => {
-  res.status(200).send('Home page');
+  console.log('Home page');
+  res.sendFile(path.resolve(__dirname, './public/_index.html'));
 });
 
+// About page.
 app.get('/about', (req, res) => {
-  res.status(200).send('About page');
+  console.log('About page');
+  res.sendFile(path.resolve(__dirname, './public/_about.html'));
 });
 
+// Everything else.
 app.all('*', (req, res) => {
   res.status(404).send('Page not found');
 });
 
+// Start the server.
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
   console.log(`Visit http://localhost:${port}`);
