be82ffba9a71086865d67421dacfbba3ae2ffa85
jimmy89Li <liviu.c.simion@gmail.com>
2025-03-24 16:56:49 +0100

    Added sample data for people

---
+++ public/data.js
@@ -0,0 +1,29 @@
+const people = [
+  {
+    id: 1,
+    name: 'John',
+    age: 30,
+  },
+  {
+    id: 2,
+    name: 'Sara',
+    age: 25,
+  },
+  {
+    id: 3,
+    name: 'Mike',
+    age: 35,
+  },
+  {
+    id: 4,
+    name: 'Anna',
+    age: 28,
+  },
+  {
+    id: 5,
+    name: 'Tom',
+    age: 40,
+  },
+];
+
+module.exports = { people };
